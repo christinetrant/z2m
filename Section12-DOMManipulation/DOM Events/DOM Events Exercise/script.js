cbf36a6bdde1ec529bc77b7eb92683140bb24bb8
Christine Trant <christinetrant@gmail.com>
2020-04-10 14:52:55 +0100

    edit item working when click on edit button and becomes uneditable when clicked on edit button again

--- Section12-DOMManipulation/DOM Events/DOM Events Exercise/script.js
+++ Section12-DOMManipulation/DOM Events/DOM Events Exercise/script.js
@@ -6,6 +6,7 @@
 
 // 3. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)
 
+// DOM Strings
 var button = document.getElementById("enter");
 var input = document.getElementById("inputText")
 var ul = document.querySelector("ul");
@@ -13,56 +14,74 @@ var li = document.querySelector("li");
 var del = document.getElementsByClassName("del");
 var edit = document.getElementsByClassName("edit");
 
+// Created DOM Strings
+
 function addListItem() {
-	// 1. Create a new list item
+	// 1. Create a new list item and span
 	var listItem = document.createElement("li");
 	var spanEl = document.createElement("span");
 	// 2. Get value of the input
 	var inputItem = document.createTextNode(input.value);
-	// Create Delete & Edit Buttons to add to list items
+	// 3. Create Delete & Edit Buttons to add to list items
 	var editBtn = document.createElement("button");
 	editBtn.classList.add("edit");
 	editBtn.innerHTML = "Edit";
 	var delBtn = document.createElement("button");
 	delBtn.classList.add("del");
 	delBtn.innerHTML = "Del";
-	// 3. Add value to the new list item
+	// 4. Add value to the new span item then add to list item
 	listItem.appendChild(spanEl);
 	spanEl.appendChild(inputItem);
-	// 4. Add new list item to the bottom of the ul
-	ul.appendChild(listItem);
+	// 5. Add edit and delete buttons to list item
 	listItem.appendChild(editBtn);
 	listItem.appendChild(delBtn);
-	// 5. Clear input field
+	// 6. Add new list item to the bottom of the ul
+	ul.appendChild(listItem);
+	// 7. Clear input field
 	input.value = "";
-	// focus style on input field
+	// 8. Focus style on input field
 	input.focus();
 }
 
-function editListItem(event) {
-	// console.log(event.target.className);
-	if(event.target.className === "edit") {
-		console.log(event.target.parentNode.getElementsByTagName("span")[0]);
-		if(event.target.parentNode.getElementsByTagName("span")[0]) {
-			console.log("Let's edit!");
-		} else {
-			console.log("No chance Lance");
-		}
-		// var edt = document.getElementsByTagName("span");
-		// console.log(edt);
-		// var editEl = event.target.parentNode;
-		// editEl.setAttribute("contentEditable", true);
-		// editEl.focus();
-		// if(listEl.contentEditable = "false") {
-			// listEl.contentEditable = "true";
-		// } else {
-			// listEl.contentEditable = "false";
-		// }
-		// ul.removeChild(event.target.parentNode);
-	}
+function editBtn(event) {
+	var editContent = event.target.parentNode.getElementsByTagName("span")[0];
+	
+		console.log("Let's edit!");
+		editContent.contentEditable = "true";
+		editContent.focus();
+		event.target.innerHTML = "Tick";
+		event.target.classList.remove('edit');
+		event.target.classList.add('tick');
+	
 }
 
-// function createDelBtn() {}
+function UneditBtn(event) {
+var editContent = event.target.parentNode.getElementsByTagName("span")[0];
+		editContent.contentEditable = "false";
+		event.target.innerHTML = "Edit";
+		event.target.classList.remove('tick');
+		event.target.classList.add('edit');
+	
+}
+
+// Edit an existing list item
+function editListItem(event) {
+	// console.log(event.target.className);
+	var editContent = event.target.parentNode.getElementsByTagName("span")[0];
+	if(editContent) {
+		if(event.target.className === "edit") {
+			console.log(editContent);
+			editBtn(event);
+		} else if(event.target.className === "tick") {
+			UneditBtn(event);
+		} 
+		} else {
+			console.log("No chance Lance");
+		
+	} 
+	
+}
+
 function toggleListItem(event) {
 	// console.log(event.target);
 	if(event.target.tagName === "SPAN") {
