758a6d364e88dc034e81357a7dd9121d96aefba4
Christine Trant <christinetrant@gmail.com>
2020-04-15 20:19:53 +0100

    only worked for first li so fixed to be fully working

--- Section12-DOMManipulation/DOM Events/DOM Events Exercise/script.js
+++ Section12-DOMManipulation/DOM Events/DOM Events Exercise/script.js
@@ -4,8 +4,7 @@
 // 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
 // 3. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)
 // 4. MY PESONAL BONUS: Make existing items editable
-
-// NEED TO DIABLE OTHER BUTTONS WHEN IN EDIT MODE
+// NEED TO DISABLE OTHER BUTTONS WHEN IN EDIT MODE
 
 // DOM Strings
 var button = document.getElementById("enter");
@@ -50,10 +49,9 @@ function addListItem() {
 	clearInput();
 }
 
-// Edit an existing list item [FIRST STEP]
+// Edit an existing list item [FIRST STEP - OUT OF THREE]
 function editListItem(event) {
 	var editContent = event.target.parentNode.getElementsByTagName("span")[0];
-	console.log(editContent);
 	// If editContent is true while in it:
 	if(editContent) {
 		// if edit mode hasn't been selected yet
@@ -64,18 +62,20 @@ function editListItem(event) {
 			if(inputValueLength() > 0) {
 				confirmEdit(event);
 			} else {
-				console.log("Can't be empty!")
+				alert("Can't be empty!")
 				editItem(event);
 			}
 		} 
 	} else {
-		console.log("No chance Lance");
+		alert("No chance Lance");
 	} 
 }
 
-// EDIT LIST ITEM [SECOND STEP]
+// EDIT LIST ITEM [SECOND STEP - OUT OF THREE]
 function editItem(event) {
 	var editContent = event.target.parentNode.getElementsByTagName("span")[0];
+	editContent.parentNode.classList.add('current');
+	console.log(editContent.parentNode);
 	// 1. Have the input equal the text so it can be edited
 	input.value = editContent.textContent;
 	// 2. Focus on the input area
@@ -85,37 +85,38 @@ function editItem(event) {
 	// Change classes from edit to tick[e.g. confirm]
 	event.target.classList.remove('edit');
 	event.target.classList.add('tick');
-	// Hide DELETE & ENTER button while editing
-	// event.target.parentNode.getElementsByClassName("del")[0].classList.add('hidden');
+	// Hide UL list while editing
 	ul.classList.add('opacity');
-	// button.classList.add('hidden');
 	// Remove event listener for AddAfterEnter key as we need a new action - updateAfterEnter
 	input.removeEventListener("keypress", addAfterEnter);
 	button.removeEventListener("click", addAfterClick);
 	ul.removeEventListener("click", toggleListItem);
 	ul.removeEventListener("click", deleteListItem);
 	ul.removeEventListener("click", editListItem);
-	console.log("addAfterEnter Event Listener removed");
+	// console.log("addAfterEnter Event Listener removed");
 	input.addEventListener("keypress", updateAfterEnter);
 	button.addEventListener("click", updateAfterClick);
 }
 
-// UPDATE LIST ITEM [THIRD STEP]
+// UPDATE LIST ITEM [THIRD STEP - OUT OF THREE]
 function confirmEdit(event) {
-	var editContent = event.target.parentNode.getElementsByTagName("span")[0];
+	var current = document.getElementsByClassName("current");
+	var editContent = current[0].getElementsByTagName("span")[0];
 	// The list item is updated
 	editContent.textContent = input.value;
+	// Remove the "current" class from the item
+	current[0].classList.remove("current");
 	// Need to change all buttons to regular state as they don't change on enter key
 	button.innerHTML = "Enter";
+	// Change classes back to normal state
 	tickBtn.classList.remove("tick");
 	tickBtn.classList.add("edit");
-	// hide unnecessary buttons
-	// delBtn.classList.remove("hidden");
-	// button.classList.remove('hidden');
+	// Show UL list after updated edit
+	ul.classList.remove('opacity');
 	// Remove event listener for AddAfterEnter key as we need a new action - updateAfterEnter
 	input.removeEventListener("keypress", updateAfterEnter);
 	button.removeEventListener("click", updateAfterClick);
-	console.log("addAfterEnter Event Listener reinstated");
+	// console.log("addAfterEnter Event Listener reinstated");
 	input.addEventListener("keypress", addAfterEnter);
 	button.addEventListener("click", addAfterClick);
 	ul.addEventListener("click", toggleListItem);
@@ -125,11 +126,9 @@ function confirmEdit(event) {
 	clearInput();
 }
 
-
 function toggleListItem(event) {
 	if(event.target.tagName === "SPAN") {
-			console.log("Is this working?");
-			event.target.classList.toggle("done");
+		event.target.classList.toggle("done");
 	}
 }
 
@@ -145,20 +144,17 @@ function inputValueLength() {
 
 function addAfterClick() {
 	if(inputValueLength() > 0) {
-		// console.log("click click!");
 		addListItem();
 	}
 }
 function addAfterEnter(event) {
 	if(inputValueLength() > 0 && event.which === 13) {
-		// console.log("enter!");
 		addListItem();
 	}
 }
 function updateAfterClick() {
 	if(inputValueLength() > 0) {
 		confirmEdit(event);
-		console.log("click click!");
 	}
 }
 function updateAfterEnter(event) {
@@ -166,14 +162,10 @@ function updateAfterEnter(event) {
 		confirmEdit(event);
 	}
 }
-
+// Event Listeners
 button.addEventListener("click", addAfterClick);
-
 input.addEventListener("keypress", addAfterEnter);
-
 ul.addEventListener("click", toggleListItem);
-
 ul.addEventListener("click", deleteListItem);
-
 ul.addEventListener("click", editListItem);
 
