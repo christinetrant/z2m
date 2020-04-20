932fb044d8aa380d785dd16764d3f586035fbd6d
Christine Trant <christinetrant@gmail.com>
2020-04-20 13:52:45 +0100

    have radial and linear buttons working alongside the input event listener

--- Section12-DOMManipulation/Background Generator/script.js
+++ Section12-DOMManipulation/Background Generator/script.js
@@ -3,64 +3,62 @@ var body = document.querySelector('body');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 
-var h3Type = document.getElementsByClassName('type')[0];
+var h3Title = document.getElementsByClassName('type')[0];
 var h3Output = document.getElementsByClassName('output')[0];
 
-var button = document.querySelector('button');
 
 var radialBtn = document.getElementById('radialBtn');
 var linearBtn = document.getElementById('linearBtn');
 // var twoColors = document.getElementById('twoColorBtn');
 // var threeColors = document.getElementById('threeColorBtn');
-
+var button = document.getElementsByClassName('buttons')[0];
+console.log(button.getElementsByClassName('btn')[0]);
+console.log(linearBtn.classList.value);
 // body.style.backgroundImage = radial-gradient(#e66465, #9198e5);
 // Linear Gradient Function
 function linear() {
-	var lBg = "linear-gradient(to right, " + color1.value +", " +color2.value +")";
+	var linearBg = "linear-gradient(to right, " + color1.value +", " +color2.value +")";
 
-	linearBtn.style.backgroundImage = lBg;
-
-	body.style.backgroundImage = lBg;
+	// linearBtn.style.backgroundImage = linearBg;
+	body.style.backgroundImage = linearBg;
 
 	linearBtn.classList.add('active');
 	radialBtn.classList.remove('active');
 	radialBtn.style.backgroundImage = "none";
+
+	// Display color values in h3 tag
+	h3Title.textContent = "Linear Gradient: ";
+	h3Output.textContent = color1.value + "   ➡   " + color2.value;
 }
 // Radial Gradient Function
 function radial() {
-	var rBg = "radial-gradient(" + color1.value +", " + color2.value + ")"; 
-	
-	radialBtn.style.backgroundImage = rBg;
-	body.style.backgroundImage = rBg;
+var radialBg = "radial-gradient(" + color1.value +", " + color2.value + ")";	
+	// radialBtn.style.backgroundImage = radialBg;
+	body.style.backgroundImage = radialBg;
 
 	radialBtn.classList.add('active');
 	linearBtn.classList.remove('active');
 	linearBtn.style.backgroundImage = "none";
-}
-// Two Colors
 
-// Three Colors
-
-function colorInput() {
 	// Display color values in h3 tag
-	h3Type.textContent = "Linear Gradient: ";
+	h3Title.textContent = "Radial Gradient: ";
 	h3Output.textContent = color1.value + "   ➡   " + color2.value;
+	// h3Display();
+}
+function colorInput() {
+
+	if(linearBtn.className === ('btn active')) {
+		console.log("linear active");
+		linear();
+	} else if(radialBtn.className === ('btn active')) {
+		console.log("radial active");
+		radial();
+	}
+	// linear();
 	// Change the background to have new colors
 	console.log(body, h3Output);
-	body.style.backgroundImage = "linear-gradient(to right, " + color1.value +", " +color2.value +")";
-	// // for(var i=0; i<button.length;i++) {
-		// linearBtn.style.backgroundImage = "linear-gradient(to right, " + color1.value +", " +color2.value +")";
-	// }
+	
 }
-// function hover() {
-// 	console.log("mouse?");
-// 	radialBtn.style.backgroundImage = "linear-gradient(to right, " + color1.value +", " +color2.value +")"
-// }
-// function nohover() {
-// 	radialBtn.style.backgroundImage = "none"
-// }
-// document.getElementById('radialBtn').addEventListener('mouseenter', hover);
-// document.getElementById('radialBtn').addEventListener('mouseleave', nohover);
 
 // HAVE TRIPLE GRADIENT OPTION?
 // #3a1c71 → #d76d77 → #ffaf7b
@@ -72,4 +70,7 @@ radialBtn.addEventListener('click', radial);
 
 
 // want linear to be the initial view
-// linear();
+linear();
+
+console.log(linearBtn.className === ('btn active'));
+console.log(radialBtn.className === ('btn active'));
