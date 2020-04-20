77eabca21049134f7265acddba6231eb01990a2d
Christine Trant <christinetrant@gmail.com>
2020-04-20 17:01:12 +0100

    have hover effect for buttons working - now trying to implement option for two or three colors

--- Section12-DOMManipulation/Background Generator/script.js
+++ Section12-DOMManipulation/Background Generator/script.js
@@ -4,73 +4,105 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 
 var h3Title = document.getElementsByClassName('type')[0];
-var h3Output = document.getElementsByClassName('output')[0];
+var h3Colors = document.getElementsByClassName('output')[0];
+var button = document.getElementsByClassName('btn');
 
+var twoColorDiv = document.getElementById('twoColorInput');
+var threeColorDiv = document.getElementById('threeColorInput');
 
 var radialBtn = document.getElementById('radialBtn');
 var linearBtn = document.getElementById('linearBtn');
-// var twoColors = document.getElementById('twoColorBtn');
-// var threeColors = document.getElementById('threeColorBtn');
-var button = document.getElementsByClassName('buttons')[0];
-console.log(button.getElementsByClassName('btn')[0]);
-console.log(linearBtn.classList.value);
-// body.style.backgroundImage = radial-gradient(#e66465, #9198e5);
+var twoColorBtn = document.getElementById('twoColorBtn');
+var threeColorBtn = document.getElementById('threeColorBtn');
+
+console.log(twoColorDiv, twoColorBtn);
+
+
 // Linear Gradient Function
 function linear() {
 	var linearBg = "linear-gradient(to right, " + color1.value +", " +color2.value +")";
 
-	// linearBtn.style.backgroundImage = linearBg;
+	// Change the background to have new colors
 	body.style.backgroundImage = linearBg;
-
+	// Change the classes so the linear button has active class
 	linearBtn.classList.add('active');
 	radialBtn.classList.remove('active');
-	radialBtn.style.backgroundImage = "none";
 
 	// Display color values in h3 tag
 	h3Title.textContent = "Linear Gradient: ";
-	h3Output.textContent = color1.value + "   ➡   " + color2.value;
+	h3Colors.textContent = color1.value + "   ➡   " + color2.value;
 }
+
 // Radial Gradient Function
 function radial() {
 var radialBg = "radial-gradient(" + color1.value +", " + color2.value + ")";	
-	// radialBtn.style.backgroundImage = radialBg;
+	// Change the background to have new colors
 	body.style.backgroundImage = radialBg;
-
+	// Change the classes so the radial button has active class
 	radialBtn.classList.add('active');
 	linearBtn.classList.remove('active');
-	linearBtn.style.backgroundImage = "none";
 
 	// Display color values in h3 tag
 	h3Title.textContent = "Radial Gradient: ";
-	h3Output.textContent = color1.value + "   ➡   " + color2.value;
-	// h3Display();
+	h3Colors.textContent = color1.value + "   ➡   " + color2.value;
 }
-function colorInput() {
 
-	if(linearBtn.className === ('btn active')) {
-		console.log("linear active");
-		linear();
-	} else if(radialBtn.className === ('btn active')) {
-		console.log("radial active");
-		radial();
-	}
-	// linear();
-	// Change the background to have new colors
-	console.log(body, h3Output);
+
+function twoColor() {
+	twoColorBtn.classList.add('active');
+	threeColorBtn.classList.remove('active');
+	twoColorDiv.classList.remove('hidden');
+	threeColorDiv.classList.add('hidden');
 	
 }
+function threeColor() {
+	threeColorBtn.classList.add('active');
+	threeColorDiv.classList.remove('hidden');
+
+	twoColorBtn.classList.remove('active');
+	twoColorDiv.classList.add('hidden');
+}
+// When the color is changed through the input element
+function colorInput() {
+	// If the linear button is active we want the output to be linear gradient
+	if(linearBtn.className === ('btn active')) {
+		linear();
+	// otherwise we'd like it to be a radial gradient 
+	} else if(radialBtn.className === ('btn active')) {
+		radial();
+	}	
+}
+
+// Hover over buttons
+function hover(event) {
+	if(linearBtn.className === ('btn active')) {
+		event.target.style.backgroundImage = "linear-gradient(to right, " + color1.value +", " +color2.value +")"
+		linearBtn.style.backgroundImage= "none";
+	} else if(radialBtn.className === ('btn active')) {
+		event.target.style.backgroundImage = "radial-gradient(" + color1.value +", " +color2.value +")"
+		radialBtn.style.backgroundImage= "none";
+	}
+}
+function nohover(event) {
+	event.target.style.backgroundImage = "none"
+}
+
 
 // HAVE TRIPLE GRADIENT OPTION?
 // #3a1c71 → #d76d77 → #ffaf7b
 
-// Event Listeners
+// EVENT LISTENERS
 document.addEventListener('input', colorInput);
 linearBtn.addEventListener('click', linear);
 radialBtn.addEventListener('click', radial);
+twoColorBtn.addEventListener('click', twoColor);
+threeColorBtn.addEventListener('click', threeColor);
+// Loop through all buttons and add gradient effect on hover and remove when mouse leaves
+for(var i=0; i<button.length; i++) {
+	button[i].addEventListener('mouseenter', hover);
+	button[i].addEventListener('mouseleave', nohover);
+}
 
-
-// want linear to be the initial view
+// want linear gradient & two color option to be the initial view
 linear();
-
-console.log(linearBtn.className === ('btn active'));
-console.log(radialBtn.className === ('btn active'));
+twoColor();
