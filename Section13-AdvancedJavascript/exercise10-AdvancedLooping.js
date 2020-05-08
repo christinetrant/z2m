307ea600e48b847d36d370a9d73ba40a2749d5e7
Christine Trant <christinetrant@gmail.com>
2020-05-08 14:09:05 +0100

    finished exercise 10 - advanced loops

--- Section13-AdvancedJavascript/exercise10-AdvancedLooping.js
+++ Section13-AdvancedJavascript/exercise10-AdvancedLooping.js
@@ -9,50 +9,96 @@ const detailedBasket = {
 for (let i = 0; i < basket.length; i++) {
   console.log(basket[i]);
 }
+// apples
+// oranges
+// grapes
 
 //2
 basket.forEach(item => {
   console.log(item);
 })
+// apples
+// oranges
+// grapes
 
-for (item in detailedBasket) {
-  console.log(item);
-}
-
+// for of
+// Iterating - arrays, strings
 for (item of basket) {
   console.log(item);
 }
+// apples
+// oranges
+// grapes
+
+// for in - (properties in objects)
+// enumerating - objects
+for (item in detailedBasket) {
+  console.log(item);
+}
+// apples
+// oranges
+// grapes
+
+
 
 // Question #1:
-// create a function called biggestNumberInArray() that takes
-// an array as a parameter and returns the biggest number.
+// create a function called biggestNumberInArray() that takes an array as a parameter and returns the biggest number.
 // biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
 // Use at least 3 different types of javascript loops to write this:
-const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array = [-1, 0, 3, 100, 99, 2, 99] // should return 100
 const array2 = ['a', 3, 4, 2] // should return 4
 const array3 = [] // should return 0
 
 function biggestNumberInArray(arr) {
-
+  let largest=0;
+  arr.forEach((value) => {
+    if(value > largest) {
+      largest = value;
+    }
+  })
+  return largest;
 }
 
 function biggestNumberInArray2(arr) {
-
-}
+  let largest=0;
+  for (value of arr) {
+    if(value > largest) {
+      largest = value;
+    }
+  }
+  return largest;
+};
 
 function biggestNumberInArray3(arr) {
-
+  let largest=0;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
 }
 
 
 // Question #2:
 // Write a function checkBasket() that lets you know if the item is in the basket or not
-amazonBasket = {
+const amazonBasket = {
   glasses: 1,
   books: 2,
   floss: 100
 }
 
 function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    // console.log(item, lookingFor)
+    if(item === lookingFor) {
+      return true
+    } else { return false }
+  }
+}
 
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    return (item === lookingFor) ? true : false
+  }
 }
