2d303f45a610961c5403e3ec3f0d58512f691bb2
Christine Trant <christinetrant@gmail.com>
2020-06-09 16:54:43 +0100

    destructuring in Card.js to make cleaner

---
+++ Section19-React/robofriends/src/Card.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import './Card.css';
+
+// const Card = (props) => {
+const Card = ({ name, email, id }) => {
+	// destructuring but we can put below in parameter above instead of props
+	// const { name, email, id } = props
+	return (
+		<Fragment>
+			<div className='athelas bg-light-purple br3 tc dib pa2 ma2 grow shadow-5 bw2'>
+				<img alt='robot' src={`https://robohash.org/${id}/set_set4/?size=200x200`}/>
+				<div>
+					<h1 className='f4'>{name}</h1>
+					<p>{email}</p>
+				</div>
+			</div>
+		</Fragment>
+	);
+}
+
+export default Card;
