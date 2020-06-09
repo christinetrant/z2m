64cb71dc96836c766a063d74f005f321392004c6
Christine Trant <christinetrant@gmail.com>
2020-06-09 17:38:43 +0100

    moved CardArray code into CardList so it is cleaner

--- Section19-React/robofriends/src/CardList.js
+++ Section19-React/robofriends/src/CardList.js
@@ -4,19 +4,31 @@ import Card from './Card';
 const CardList = ({robots}) => {
 	// rather than calling each card by index we map to create a new array
 	// In the console we get an error: Warning: Each child in a list should have a unique "key" prop. so we add a unique key e.g. the id
-	const CardArray = robots.map((user, i) => {
-		return (
-			<Card 
-				key={robots[i].id} 
-				id={robots[i].id} 
-				name={robots[i].name} 
-				email={robots[i].email} 
-			/>
-		)
-	})
+	// const CardArray = robots.map((user, i) => {
+	// 	return (
+	// 		<Card 
+	// 			key={robots[i].id} 
+	// 			id={robots[i].id} 
+	// 			name={robots[i].name} 
+	// 			email={robots[i].email} 
+	// 		/>
+	// 	)
+	// })
 	return (
 		<div>
-			{ CardArray }
+			{/*{ CardArray }*/}
+			{
+				robots.map((user, i) => {
+					return (
+						<Card 
+							key={robots[i].id} 
+							id={robots[i].id} 
+							name={robots[i].name} 
+							email={robots[i].email} 
+						/>
+					)
+				})
+			}
 			{/*<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
 				    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
 				    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />*/}
