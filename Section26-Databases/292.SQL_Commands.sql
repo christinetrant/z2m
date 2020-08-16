cde7aa67d7203e14966d63ee6082ad5923789c21
Christine Trant <christinetrant@gmail.com>
2020-08-16 11:27:17 +0100

    Finished SQL exercises

--- Section26-Databases/292.SQL_Commands.sql
+++ Section26-Databases/292.SQL_Commands.sql
@@ -1,89 +1,91 @@
-## SQL Commands
+-- SQL Commands
 
 
-### To create a database
+-- To create a database
 CREATE DATABASE DatabaseName;
 
-### To create a table
+-- To create a table
 CREATE TABLE table_name (column_1 datatype, column_2 datatype, column_3 datatype);
-e.g.,
+/*e.g.,*/
 CREATE TABLE users (name text, age smallint, birthday date);
 
-### To view table in command line
-\d
+-- To view table in command line
+`\d`
 
-### To exit PostgreSQL
-\q
+-- To exit PostgreSQL
+`\q`
 
 
-## Insert Into + Select
+-- Insert Into + Select
 
-## INSERT INTO
+-- INSERT INTO
 INSERT INTO users(name, age, birthday) VALUES ('Andrei', 31, '1930-01-25');
 
-## To view entries in a table
+-- To view entries in a table
 SELECT name, age, birthday FROM users
-
+/*
  name  | age |  birthday
 -------+-----+-----------
 Andrei |  31 | 1930-01-25
+*/
 
-## or just to view all:
+-- or just to view all:
 SELECT * FROM users;
 
 
-### SQL: Alter Table + Update
+-- SQL: Alter Table + Update
 
-## Alter Table
+-- Alter Table
 ALTER TABLE table_name ADD column datatype;
-e.g.,
+/*e.g.,*/
 ALTER TABLE users ADD score smallint;
 
-## Update a table
+-- Update a table
 UPDATE table_name
 SET some_column = some_value
 WHERE some_column = some_value;
-e.g.,
+/*e.g.,*/
 UPDATE users SET score = 50 WHERE name = 'Andrei';
 
 UPDATE users SET score = 100 WHERE name = 'John' OR name = 'Sally';
 
-### SQL: Conditional Selections
+-- SQL: Conditional Selections
 
-## To search for a name
-Searches are case sensitive 
+-- To search for a name
+/*Searches are case sensitive 
 In the below statement we are looking for a name starting with A. 
-The % means it can be anything after A
+The % means it can be anything after A*/
 SELECT * FROM users WHERE name LIKE 'A%';
 
 SELECT * FROM users WHERE name LIKE '%y';
 
-## Sort in order
+-- Sort in order
 SELECT * FROM users ORDER BY score DESC;
-
+/*
  name  | age |  birthday  | score
 -------+-----+------------+-------
 Sally  |  38 | 1985-06-04 |   100
 John   |  61 | 1930-04-04 |   100
 Amy    |  24 | 1930-12-28 |    88
 Andrei |  31 | 1930-01-25 |    50
+*/
 
 SELECT * FROM users ORDER BY score ASC;
 
 
-### SQL Functions
+-- SQL Functions
 
-### To get the average score of users:
+-- To get the average score of users:
 SELECT AVG(score) FROM users;
 
-### To get the sum of users age:
+-- To get the sum of users age:
 SELECT SUM(age) FROM users;
 
-### To find out how many users we have in our database:
+-- To find out how many users we have in our database:
 SELECT COUNT(name) FROM users;
 
 
-### Joining Tables
+-- Joining Tables
 
 CREATE TABLE login {
     ID serial NOT NULL PRIMARY KEY,
@@ -91,7 +93,7 @@ CREATE TABLE login {
     name text UNIQUE NOT NULL
 };
 
-Above we are creating a login table with 
+/*Above we are creating a login table with 
 and ID of datatype serial, so every time a new user is incremented it will add a new id +1, 
 NOT NULL means that this has to be a filled in property.
 It is also the primary key.  Primary keys are always unique to each row so ID is perfect.
@@ -99,35 +101,35 @@ It is also the primary key.  Primary keys are always unique to each row so ID is
 secret is variable character of 100.  This means it will not be greater than 100 and as it is not null it always needs to be filled in
 
 name of datatype text has to be unique.  
+*/
 
-
-## To add a row to the newly created table:
+-- To add a row to the newly created table:
 INSERT INTO login (secret, name) VALUES ('abc', 'Andrei');
 INSERT INTO login (secret, name) VALUES ('xyz', 'Sally');
 INSERT INTO login (secret, name) VALUES ('lol', 'John');
 
-We now have 2 tables with Andrei, Sally & John
-Andrei, Sally & John is the FOREIGN KEY as it is the common denominator in both tables
+/*We now have 2 tables with Andrei, Sally & John
+Andrei, Sally & John is the FOREIGN KEY as it is the common denominator in both tables*/
 
-## Joining tables
-Say we want information from users to join with login.  As name is the primary key we join using the names in both tables as the common factor:
+-- Joining tables
+/*Say we want information from users to join with login.  As name is the primary key we join using the names in both tables as the common factor:*/
 
 SELECT * FROM users JOIN login ON users.name = login.name;
-
+/*
  name  | age |  birthday  | score | id | secret | name
 -------+-----+------------+-------+----+--------+------
 Andrei |  31 | 1930-01-25 |    50 |  1 | abc    | Andrei
 Sally  |  38 | 1985-06-04 |   100 |  2 | xyz    | Sally 
 John   |  61 | 1930-04-04 |   100 |  3 | lol    | John
+*/
+/*With JOIN we can make small little tables to select specific information that we need.*/
 
-With JOIN we can make small little tables to select specific information that we need.
-
-## Delete From + Drop Table
-If Sally wants to delete her information
+-- Delete From + Drop Table
+/*If Sally wants to delete her information*/
 DELETE FROM users WHERE name = 'Sally'; 
 
 
-## To Delete a table
+-- To Delete a table
 DROP TABLE login;
 
 DROP TABLE users;
--- Section26-Databases/300.SQLExercise4-Modifying_Databases.sql/300.SQLExercise3-Relational_queries.sql
+++ Section26-Databases/300.SQLExercise4-Modifying_Databases.sql/300.SQLExercise3-Relational_queries.sql
@@ -69,6 +69,81 @@ SELECT students.first_name, students.last_name, student_projects.title
 /* So LEFT means we want all info from the left table (the FROM table)
 and we want it to retain the rows even if there is no match in the OUTER (right table) so Alice has a project title of NULL */
 
+/* JOINING TABLES TO THEMSELVES WITH SELF JOINS */
+/* created a new column - buddy_id, this is related to another column in the SAME table */
+CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone TEXT,
+    birthdate TEXT,
+    buddy_id INTEGER);
+
+INSERT INTO students 
+    VALUES (1, "Peter", "Rabbit", "peter@rabbit.com", "555-6666", "2002-06-24", 2);
+INSERT INTO students 
+    VALUES (2, "Alice", "Wonderland", "alice@wonderland.com", "555-4444", "2002-07-04", 1);
+INSERT INTO students 
+    VALUES (3, "Aladdin", "Lampland", "aladdin@lampland.com", "555-3333", "2001-05-10", 4);
+INSERT INTO students 
+    VALUES (4, "Simba", "Kingston", "simba@kingston.com", "555-1111", "2001-12-24", 3);
+    
+SELECT id, first_name, last_name, buddy_id FROM students;
+/* SELF JOIN */
+/* To select info from the same table we join them by adding an 'alias' to the JOIN */
+SELECT students.first_name, students.last_name, buddies.email as buddy_email
+    FROM students
+    JOIN students buddies
+    ON students.buddy_id = buddies.id;
+
+/* COMBINING MULTIPLE JOINS */
+CREATE TABLE students (id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone TEXT,
+    birthdate TEXT);
+
+INSERT INTO students (first_name, last_name, email, phone, birthdate)
+    VALUES ("Peter", "Rabbit", "peter@rabbit.com", "555-6666", "2002-06-24");
+INSERT INTO students (first_name, last_name, email, phone, birthdate)
+    VALUES ("Alice", "Wonderland", "alice@wonderland.com", "555-4444", "2002-07-04");
+INSERT INTO students (first_name, last_name, email, phone, birthdate)
+    VALUES ("Aladdin", "Lampland", "aladdin@lampland.com", "555-3333", "2001-05-10");
+INSERT INTO students (first_name, last_name, email, phone, birthdate)
+    VALUES ("Simba", "Kingston", "simba@kingston.com", "555-1111", "2001-12-24");
+    
+CREATE TABLE student_projects (id INTEGER PRIMARY KEY,
+    student_id INTEGER,
+    title TEXT);
+    
+INSERT INTO student_projects (student_id, title)
+    VALUES (1, "Carrotapault");
+INSERT INTO student_projects (student_id, title)
+    VALUES (2, "Mad Hattery");
+INSERT INTO student_projects (student_id, title)
+    VALUES (3, "Carpet Physics");
+INSERT INTO student_projects (student_id, title)
+    VALUES (4, "Hyena Habitats");
+    
+CREATE TABLE project_pairs (id INTEGER PRIMARY KEY,
+    project1_id INTEGER,
+    project2_id INTEGER);
+
+INSERT INTO project_pairs (project1_id, project2_id)
+    VALUES(1, 2);
+INSERT INTO project_pairs (project1_id, project2_id)
+    VALUES(3, 4);
+    
+SELECT a.title, b.title FROM project_pairs
+    JOIN student_projects a
+    ON project_pairs.project1_id = a.id
+    JOIN student_projects b
+    ON project_pairs.project2_id = b.id;
+
+
+
+
 
 -- EXERCISES
 -- CHALLENGE: BOBBY'S HOBBIES (JOIN)
@@ -130,38 +205,120 @@ SELECT persons.name, hobbies.name FROM persons
 
 -- CHALLENGE: CUSTOMER'S ORDERS (LEFT OUTER JOIN)
 CREATE TABLE customers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email TEXT);
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT);
     
 INSERT INTO customers (name, email) VALUES ("Doctor Who", "doctorwho@timelords.com");
 INSERT INTO customers (name, email) VALUES ("Harry Potter", "harry@potter.com");
 INSERT INTO customers (name, email) VALUES ("Captain Awesome", "captain@awesome.com");
 
 CREATE TABLE orders (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    customer_id INTEGER,
-    item TEXT,
-    price REAL);
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  customer_id INTEGER,
+  item TEXT,
+  price REAL);
 
 INSERT INTO orders (customer_id, item, price)
-    VALUES (1, "Sonic Screwdriver", 1000.00);
+  VALUES (1, "Sonic Screwdriver", 1000.00);
 INSERT INTO orders (customer_id, item, price)
-    VALUES (2, "High Quality Broomstick", 40.00);
+  VALUES (2, "High Quality Broomstick", 40.00);
 INSERT INTO orders (customer_id, item, price)
-    VALUES (1, "TARDIS", 1000000.00);
+  VALUES (1, "TARDIS", 1000000.00);
 
 /* Come up with a query that lists the name and email of every customer followed by the item and price of orders they've made. Use a LEFT OUTER JOIN so that a customer is listed even if they've made no orders, and don't add any ORDER BY. */
 SELECT customers.name, customers.email, orders.item, orders.price 
-FROM customers
-LEFT OUTER JOIN orders
-ON customers.id = orders.customer_id;
+  FROM customers
+  LEFT OUTER JOIN orders
+  ON customers.id = orders.customer_id;
 
 /* create another query that will result in one row per each customer, with their name, email, and total amount of money they've spent on orders. Sort the rows according to the total money spent, from the most spent to the least spent.
 (Tip: You should always GROUP BY on the column that is most likely to be unique in a row.) */
 SELECT customers.name, customers.email, SUM(orders.price)
-FROM customers
-LEFT OUTER JOIN orders
-ON customers.id = orders.customer_id
-GROUP BY customers.id
-ORDER BY orders.price DESC;
+  FROM customers
+  LEFT OUTER JOIN orders
+  ON customers.id = orders.customer_id
+  GROUP BY customers.id
+  ORDER BY orders.price DESC;
+
+
+-- CHALLENGE: SEQUELS IN SQL (SELF JOINS)
+CREATE TABLE movies (id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  released INTEGER,
+  sequel_id INTEGER);
+
+INSERT INTO movies 
+  VALUES (1, "Harry Potter and the Philosopher's Stone", 2001, 2);
+INSERT INTO movies 
+  VALUES (2, "Harry Potter and the Chamber of Secrets", 2002, 3);
+INSERT INTO movies 
+  VALUES (3, "Harry Potter and the Prisoner of Azkaban", 2004, 4);
+INSERT INTO movies 
+  VALUES (4, "Harry Potter and the Goblet of Fire", 2005, 5);
+INSERT INTO movies 
+  VALUES (5, "Harry Potter and the Order of the Phoenix ", 2007, 6);
+INSERT INTO movies 
+  VALUES (6, "Harry Potter and the Half-Blood Prince", 2009, 7);
+INSERT INTO movies 
+  VALUES (7, "Harry Potter and the Deathly Hallows – Part 1", 2010, 8);
+INSERT INTO movies 
+  VALUES (8, "Harry Potter and the Deathly Hallows – Part 2", 2011, NULL);
+
+/* We've created a table with all the 'Harry Potter' movies, with a sequel_id column that matches the id of the sequel for each movie. Issue a SELECT that will show the title of each movie next to its sequel's title (or NULL if it doesn't have a sequel). */
+SELECT movies.title, sequels.title AS next_title
+  FROM movies
+  LEFT OUTER JOIN movies sequels
+  ON movies.sequel_id = sequels.id
+
+
+-- Challenge: FriendBook (MULTIPLE JOINS)
+CREATE TABLE persons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  fullname TEXT,
+  age INTEGER);
+    
+INSERT INTO persons (fullname, age) VALUES ("Bobby McBobbyFace", "12");
+INSERT INTO persons (fullname, age) VALUES ("Lucy BoBucie", "25");
+INSERT INTO persons (fullname, age) VALUES ("Banana FoFanna", "14");
+INSERT INTO persons (fullname, age) VALUES ("Shish Kabob", "20");
+INSERT INTO persons (fullname, age) VALUES ("Fluffy Sparkles", "8");
+
+CREATE table hobbies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  person_id INTEGER,
+  name TEXT);
+    
+INSERT INTO hobbies (person_id, name) VALUES (1, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (1, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (2, "dancing");
+INSERT INTO hobbies (person_id, name) VALUES (2, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (3, "skating");
+INSERT INTO hobbies (person_id, name) VALUES (3, "rowing");
+INSERT INTO hobbies (person_id, name) VALUES (3, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (4, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (4, "dilly-dallying");
+INSERT INTO hobbies (person_id, name) VALUES (4, "meowing");
+
+CREATE table friends (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  person1_id INTEGER,
+  person2_id INTEGER);
+
+INSERT INTO friends (person1_id, person2_id)
+  VALUES (1, 4);
+INSERT INTO friends (person1_id, person2_id)
+  VALUES (2, 3);
+    
+/* use a JOIN to display a table showing people's names with their hobbies. */
+SELECT persons.fullname, hobbies.name
+ FROM persons
+ JOIN hobbies
+ ON persons.id = hobbies.person_id;
+ 
+/* use another SELECT with a JOIN to show the names of each pair of friends, based on the data in the friends table. */
+SELECT friend1.fullname, friend2.fullname    FROM friends
+  JOIN persons friend1
+  ON friend1.id = friends.person1_id
+  JOIN persons friend2
+  ON friend2.id = friends.person2_id;
