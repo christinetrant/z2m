c2ca080951abf3d9a0bcc1831462f43fc813b005
Christine Trant <christinetrant@gmail.com>
2020-08-26 13:04:09 +0100

    adding provider to redux robofriends

--- Section19-React/robofriends/src/containers/App.js
+++ Section19-React/robofriends/src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 // import { robots } from './robots';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import './App.css';
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "./App.css";
 
 // Using state
 // https://www.freecodecamp.org/news/react-js-for-beginners-props-state-explained/
@@ -14,62 +14,62 @@ import './App.css';
 // 	searchfield: ''
 // }
 
-
-// Inside state - these are the things that can change and affect our app: 
+// Inside state - these are the things that can change and affect our app:
 // State is defined in the parent and passed to Component child as props (properties)
 
 // In order to use state we need to use classes
 // const App = () => {
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			// robots: robots,
-			// we will be getting the list so we start off with an empty array
-			robots: [],
-			searchfield: ''
-		}
-	}
-	
-	// when component 'mounts' we set robots to equal robots array from robots.js:
-	// instead of fetching array we get user info from jsonplaceholder then we convert response into JSON then we set that to equal the robots array
-	componentDidMount() {
-		fetch('http://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
-		// this.setState({ robots: robots })
-	}
+  constructor() {
+    super();
+    this.state = {
+      // robots: robots,
+      // we will be getting the list so we start off with an empty array
+      robots: [],
+      searchfield: "",
+    };
+  }
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-		// const filteredRobots = this.state.robots.filter(robot => {
-		// 	return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		// })
-		// console.log(filteredRobots);
-	}
+  // when component 'mounts' we set robots to equal robots array from robots.js:
+  // instead of fetching array we get user info from jsonplaceholder then we convert response into JSON then we set that to equal the robots array
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+    // this.setState({ robots: robots })
+  }
 
-	render() {
-		// instead of calling this.state.searchfield and this.state.robots we can destructure:
-		const { robots, searchfield } = this.state
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		
-		// if(robots.length === 0) {
-		return !robots.length ?	
-			<h1 className='tc light-green'>Loading</h1>	:
-			(
-				<div className='tc'>
-					<h1 className='light-green'>RoboFriends</h1>
-					<SearchBox searchChange = {this.onSearchChange} />
-					{/*<CardList robots={robots} /> */}
-					<Scroll>
-						<ErrorBoundary>
-							<CardList robots={filteredRobots} />
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			);
-	}
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+    // const filteredRobots = this.state.robots.filter(robot => {
+    // 	return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    // })
+    // console.log(filteredRobots);
+  };
+
+  render() {
+    // instead of calling this.state.searchfield and this.state.robots we can destructure:
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+
+    // We don't want to call each card separately - what if we have 20 cards?  So we create a cardlist component
+    // if(robots.length === 0) {
+    return !robots.length ? (
+      <h1 className="tc light-green">Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="light-green">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        {/*<CardList robots={robots} /> */}
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
+  }
 }
 export default App;
--- Section19-React/robofriends/src/index.js
+++ Section19-React/robofriends/src/index.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// We need provider to make redux work with our App
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import "./index.css";
 // import CardList from './CardList';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import { searchRobots } from "./reducers";
+import "tachyons";
 // robots isn't a default app, it is a variable so we destructure when calling (curly brackets)
 // import { robots } from './robots'
 
 // if we are using javascript we wrap in curly brackets - JSX rules
 
-// We don't want to call each card separately - what if we have 20 cards?  So we create a cardlist component
+// Redux: create store to call reducers - we want to combine all reducers to create one root reducer
+const store = createStore(searchRobots);
+
+// instead of passing down the store to the app - include it into the provider component and the provider component is going to take care of passing down the store to all the components down the component tree that need it
 ReactDOM.render(
   <React.StrictMode>
-  <App />
-  {/*<CardList robots={robots} />*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+    {/*<CardList robots={robots} />*/}
     {/*<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
     <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
     <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />*/}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
